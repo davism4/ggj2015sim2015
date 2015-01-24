aeafda20c02ec269fde239f7f08b5bf5f7eec349
Michael Davis <davism4@umbc.edu>
2015-01-23 19:57:53 -0500

    Things are set up. Scenes are empty except with game controller and camera.

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGame : MonoBehaviour {
+
+	public float time; // seconds
+	public int IntTime {
+		get { return Mathf.RoundToInt(Time); }
+	}
+	
+	public string GameTitle;
+	public Texture2D ArtTexture;
+	public AudioClip[] AudioSounds;
+	
+	public float ArtQuality = 0f;
+	public float AudioQuality = 0f;
+	public float StringQuality = 0f;
+	
+	void Awake()
+	{
+		time = 48f;
+	}
+	
+	void Update()
+	{
+		time -= Time.deltaTime;
+	}
+}
