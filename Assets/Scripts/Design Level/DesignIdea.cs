5fec452e21359cf90cf99b42462871721d8bd996
Michael Davis <davism4@umbc.edu>
2015-01-23 22:49:42 -0500

    Working on the design concept level.

---
+++ Assets/Scripts/Design Level/DesignIdea.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DesignIdea : MonoBehaviour {
+
+    public string Word;
+    public bool Good = true;
+    [HideInInspector] public new Transform transform;
+    public float SpeedMin = 1.0f;
+	public float SpeedMax = 1.5f;
+	Text text;
+	
+	void Awake()
+    {
+    	transform = GetComponent<Transform>();
+        rigidbody2D.velocity = -Vector2.up * UnityEngine.Random.Range(SpeedMin, SpeedMax);
+        text = GetComponent<Text>();
+	}
+	
+	public void SetWord(string w)
+	{
+		Word = w;
+		text.text = Word;
+	}
+	
+	void Update ()
+    {
+        if (transform.position.y < -20f)
+        {
+        	Destroy(this.gameObject);
+        }
+        text.transform.position = this.transform.position;
+	}
+	
+}
--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,28 +1,23 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class MainGame : MonoBehaviour {
 
-	public float time; // seconds
-	public int IntTime {
-		get { return Mathf.RoundToInt(Time); }
-	}
+	public static float time; // seconds
 	
-	public string GameTitle;
-	public Texture2D ArtTexture;
-	public AudioClip[] AudioSounds;
-	
-	public float ArtQuality = 0f;
-	public float AudioQuality = 0f;
-	public float StringQuality = 0f;
-	
-	void Awake()
-	{
-		time = 48f;
-	}
-	
+	public static string GameTitle;
+	public static Texture2D ArtTexture;
+	public static AudioClip[] AudioSounds;
+
+    public static float CodeQuality = 0;
+    public static float ArtQuality = 0;
+    public static float AudioQuality = 0;
+    public static float DesignQuality = 0;
+
 	void Update()
 	{
 		time -= Time.deltaTime;
 	}
+
 }
